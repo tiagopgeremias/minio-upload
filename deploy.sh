3315e12a4a669bb6ade5424435469ee87996c188
Tiago Geremias <tiagopgeremias@gmail.com>
2020-02-17 17:17:03 -0300

    Criando o Script de deploy

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Deploy minio-upload
+#
+# Por: Tiago Geremias
+# <https://github.com/tiagopgeremias>  
+# <tiagopgeremias@gmail.com>
+#
+# 
+#
+#
+
+if [ $(whoami) != "root" ];
+then
+  echo "Erro ao executar o deploy."
+  echo "Execute: sudo $0/deploy.sh"
+  exit 1
+fi
+
+# Finalizando o processo do Gunicorn
+if [ $(ps -ef | grep gunicorn | grep -v grep | wc -l) -gt 0 ];
+then
+  kill -9 $(ps -ef | grep gunicorn | grep -v grep)
+fi
+
+echo "Deploy finalizado com sucesso."
+
+
