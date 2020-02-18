219a1d8e043da9791631be398005182d98f881fe
Tiago Geremias <tiagopgeremias@gmail.com>
2020-02-18 17:22:25 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,37 +11,37 @@
 
 if [ $(whoami) != "root" ];
 then
-  echo "Erro ao executar o deploy."
-  echo "Execute: sudo $0"
+  echo "  =>  Erro ao executar o deploy."
+  echo "  ==> Execute: sudo $0"
   exit 1
 fi
 
 cd /opt/minio-upload
 
-echo "Carregando o arquivo de Environment"
+echo "  =>  Carregando o arquivo de Environment"
 if [ -f .env ];
 then
   source .env
   echo "Arquivo Environment carregado"
 else
-  echo "Arquivo de .env nao encontrado"
-  echo " => Execute o playbook de provisionamento"
-  echo "https://github.com/tiagopgeremias/provising-minio-app"
+  echo "  =>  Arquivo de .env nao encontrado"
+  echo "  ==>  Execute o playbook de provisionamento"
+  echo "  ==>  https://github.com/tiagopgeremias/provising-minio-app"
   exit 1
 fi
 
-echo "Finalizando o processo do gunicorn"
+echo "  =>  Instalando dependencias do projeto"
+pipenv --python 3 install --system --deploy
+
+echo "  =>  Finalizando o processo do gunicorn"
 if [ $(ps -ef | grep gunicorn | grep -v grep | wc -l) -gt 0 ];
 then
   kill -9 $(ps -ef | grep gunicorn | grep -v grep | awk '{print $2}')
 fi
 
-echo "Instalando dependencias do projeto"
-pipenv --python 3 install --system --deploy
-
-echo "Iniciando a aplicação"
+echo "  =>  Iniciando a aplicação"
 gunicorn --chdir $(pwd) --bind 0.0.0.0:5000 wsgi:app --daemon
 
-echo "Deploy finalizado com sucesso."
+echo "  =>  Deploy finalizado com sucesso."
 
 
